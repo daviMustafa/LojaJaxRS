c819b2d6e8d1ea45cca69ba8ad1751ead95a3930
Davi Mustafa de Oliveira <davi.oliveira@arce.ce.gov.br>
2016-07-06 13:40:53 -0300

    Segundo Capitulo do curso JAX-RS e Jersey Alura

--- src/main/java/br/com/alura/loja/Servidor.java
+++ src/main/java/br/com/alura/loja/Servidor.java
@@ -1,6 +1,5 @@
 package br.com.alura.loja;
 
-import java.io.IOException;
 import java.net.URI;
 
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -9,12 +8,17 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Servidor {
 	
-	public static void main(String[] args) throws IOException {
-		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-		URI uri = URI.create("http://localhost:8080/");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+	public static void main(String[] args) throws Exception {
+		HttpServer server = inicializaServidor();
 		System.out.println("Servidor rodando");
 		System.in.read();
 		server.stop();
+	}
+
+	public static HttpServer inicializaServidor() {
+		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
+		URI uri = URI.create("http://localhost:8080/");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		return server;
 	}
 }
--- src/test/java/br/com/alura/loja/ProjetoTest.java
+++ src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -4,8 +4,15 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Projeto;
 
 /**
  * @author ARCE - Davi Mustafa
@@ -13,13 +20,25 @@ import org.junit.Test;
  */
 public class ProjetoTest {
 	
+	private HttpServer server;
+	
+	@Before
+	public void startaServidor(){
+		server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void mataServidor(){
+		server.stop();
+	}
+	
 	@Test
 	public void testaQueAConexaoComOServidorFunciona(){
 		
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/projetos");
+		WebTarget target = client.target("http://localhost:8080");
 		String conteudo = target.path("/projetos").request().get(String.class);
-		
-		Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+		Projeto projeto = ((Projeto) new XStream().fromXML(conteudo));
+		Assert.assertEquals("Minha loja", projeto.getNome());
 	}
 }
